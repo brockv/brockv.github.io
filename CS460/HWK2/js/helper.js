a605368021756f0192826c77a14e0d3d2709970e
brockv <vancebrock22@gmail.com>
2018-10-02 13:23:31 -0700

    Refactors.

--- CS460/HWK2/js/helper.js
+++ CS460/HWK2/js/helper.js
@@ -1,6 +1,6 @@
 /*eslint-env browser*/
 
-// Initialize variables used for maintaining the to do list
+// Initialize variables used for maintaining the to-do list
 var toDoList      = document.querySelector("ul"),
     item          = document.getElementsByTagName("li"),
     userInput     = document.getElementById("userInput"),
@@ -12,13 +12,13 @@ function userInputLength() {
     return userInput.value.length;
 }
 
-// Helper function that gets the length of the to do list
-function toDoListLength() {
+// Helper function that gets the length of a given list item
+function newItemLength() {
     "use strict";
     return item.length;
 }
 
-// Function that handles actions related to the to do list
+// Function that handles actions related to the to-do list
 function addNewItem() {
     "use strict";
     // Create a new list item
@@ -28,7 +28,7 @@ function addNewItem() {
     // Assign the user's input to the new list item
     li.appendChild(document.createTextNode(userInput.value));
     
-    // Append the new list item to the to do list
+    // Append the new list item to the to-do list
     toDoList.appendChild(li);
     
     // Reset the text field for the user's convenience
@@ -57,7 +57,7 @@ function addNewItem() {
 // Event handler for when the user clicks the button
 function addItemAfterClick() {
     "use strict";
-	if (toDoListLength() > 0) {
+	if (newItemLength() > 0) {
 		addNewItem();
 	}
 }
