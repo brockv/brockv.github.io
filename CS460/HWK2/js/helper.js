3420b5c3a146ba47882426bacaad3fbd84ef1b31
brockv <vancebrock22@gmail.com>
2018-10-02 22:03:05 -0700

    Started converting JS functions to jQuery.

--- CS460/HWK2/js/helper.js
+++ CS460/HWK2/js/helper.js
@@ -1,27 +1,15 @@
-// Needed to stop the script from freaking out about getting information from the document
 /*eslint-env browser*/
+/*jslint browser: true*/
+/*global $ */
 
 // Initialize variables used for maintaining the to-do list
-var toDoList      = document.querySelector("ul"),
-    item          = document.getElementsByTagName("li"),
-    userInput     = document.getElementById("userInput"),
-    addItemButton = document.getElementById("btnAddItem");
+//var toDoList      = document.querySelector("ul"),
+var    userInput     = document.getElementById("userInput");
 
 // Helper function that gets the length of the user's input
 function userInputLength() {
     "use strict";
     return userInput.value.length;
-}
-
-<<<<<<< HEAD
-// Helper function that gets the length of the to-do list
-function toDoListLength() {
-=======
-// Helper function that gets the length of a given list item
-function newItemLength() {
->>>>>>> ef9210f3d03e5e0716f4c01b79f459f2df188b7d
-    "use strict";
-    return item.length;
 }
 
 // Function that handles actions related to the to-do list
@@ -35,14 +23,15 @@ function addNewItem() {
     li.appendChild(document.createTextNode(userInput.value));
     
     // Append the new list item to the to-do list
-    toDoList.appendChild(li);
+    $("ul").append(li);
+    //toDoList.appendChild(li);
     
     // Reset the text field for the user's convenience
-    userInput.value = "";
+    $("#userInput").val('');
     
-    // Add the 'checked' class to newly created items
+    // Add the 'completed' class to newly created items
 	function markCompleted() {
-		li.classList.toggle("checked");
+		li.classList.toggle("completed");
 	}
 
 	li.addEventListener("click", markCompleted);
@@ -59,24 +48,24 @@ function addNewItem() {
 
 }
 
-// Event handler for when the user clicks the button
-function addItemAfterClick() {
+// Called once the page is loaded
+function main() {
     "use strict";
-	if (newItemLength() > 0) {
-		addNewItem();
-	}
+    // Add an event handler for when the user clicks the button
+    $("#btnAddItem").on('click', function () {
+        if (userInputLength() > 0) {
+            addNewItem();
+        }
+    });
+    
+    // Add an event handler for when the user presses 'Enter'
+    $("#userInput").keypress(function () {
+        if (userInputLength() > 0 && event.which === 13) {
+            addNewItem();
+        }
+    });
 }
 
-// Event handler for when the user presses 'Enter'
-function addItemAfterKeypress(event) {
-    "use strict";
-	if (userInputLength() > 0 && event.which === 13) {
-		addNewItem();
-	}
-}
+// Call the main function once the page is loaded
+$(document).ready(main);
 
-// Add an event listener for when the user clicks on the button
-addItemButton.addEventListener("click", addItemAfterClick);
-
-// Add an event listener for when the user presses 'Enter'
-userInput.addEventListener("keypress", addItemAfterKeypress);
