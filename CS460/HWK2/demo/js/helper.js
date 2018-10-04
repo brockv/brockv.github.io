3a2630b9f8db2aeb008931aee21df39d90688d26
brockv <vancebrock22@gmail.com>
2018-10-04 14:03:19 -0700

    Updated the lists -- Completed tasks now move to a separate list.

--- CS460/HWK2/demo/js/helper.js
+++ CS460/HWK2/demo/js/helper.js
@@ -1,18 +1,38 @@
 /*eslint-env browser*/
 /*jslint browser: true*/
-/*global $ */
+/*global $*/
 
 // Grab the input field from the form for later use
 var userInput = document.getElementById("userInput");
 
 // Helper function that gets the length of the user's input
 function userInputLength() {
+    // Enabled to shut the editor up
     "use strict";
+    
+    // Return the length of the input
     return userInput.value.length;
+}
+
+// Move items between the two lists
+function toggleCompleted(li) {
+    // Enabled to shut the editor up
+    "use strict";
+
+    // Toggle the visuals for incomplete / completed items
+    li.classList.toggle("completed");
+
+    // Move the item to the appropriate list
+    if ($(li).hasClass("completed")) {
+        $(li).appendTo("#completedTasks");
+    } else {
+        $(li).appendTo("#incompleteTasks");
+    }
 }
 
 // Function that handles actions related to the to-do list
 function addNewItem() {
+    // Enabled to shut the editor up
     "use strict";
     // Create a new list item and a button to delete it
     var li            = document.createElement("li"),
@@ -22,19 +42,14 @@ function addNewItem() {
     $(li).append(document.createTextNode(userInput.value));
     
     // Append the new list item to the to-do list
-    $("ul").append(li);
+    $("#incompleteTasks").append(li);
     
     // Reset the text field for the user's convenience
-    $("#userInput").val('');
-    
-    // Add the 'completed' class to newly created items
-	function markCompleted() {
-		li.classList.toggle("completed");
-	}
-
+    $("#userInput").val("");
+        
     // Add an event listener to each newly created item for toggling completion
     $(li).click(function () {
-        markCompleted();
+        toggleCompleted(li);
     });
     
     // Add the 'delete' class to newly created items
@@ -53,6 +68,7 @@ function addNewItem() {
 
 // Called once the page is loaded
 function main() {
+    // Enabled to shut the editor up
     "use strict";
     // Called when the user clicks the button to add an item
     $("#btnAddItem").on('click', function () {
