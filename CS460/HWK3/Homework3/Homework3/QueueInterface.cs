029f9fb53c4eb85aede083742707aa88338c3bf5
brockv <vancebrock22@gmail.com>
2018-10-08 23:54:54 -0700

    Added QueueInterface.cs

---
+++ CS460/HWK3/Homework3/Homework3/QueueInterface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary>
+/// A FIFO queue interface. This ADT is suitable for a singly-linked
+/// queue.
+/// </summary>
+/// <typeparam name="T">The element type.</typeparam>
+public interface QueueInterface<T>
+{
+    /// <summary>
+    /// Add an element to the rear of the queue
+    /// </summary>
+    /// <param name="element">The element to add</param>
+    /// <returns> The element that was enqueued</returns>
+    T Push(T element);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    T Pop();
+    
+    /// <summary>
+    /// Test if the queue is empty
+    /// </summary>
+    /// <returns>True if the queue is empty; otherwise false</returns>
+    bool IsEmpty();
+}
