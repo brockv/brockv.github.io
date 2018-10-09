70bf7d989cfacb01198b08b459bc5e0552bbe208
brockv <vancebrock22@gmail.com>
2018-10-08 23:17:49 -0700

    Added Node.cs

---
+++ CS460/HWK3/Homework3/Homework3/Node.cs
@@ -0,0 +1,22 @@
+﻿
+
+/// <summary>
+/// Singly-linked node class.
+/// </summary>
+/// <typeparam name="T">The element type of the node.</typeparam>
+public class Node<T>
+{
+    public T data;
+    public Node<T> next;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="data">The contents of the node.</param>
+    /// <param name="next">The node this one points to.</param>
+	public Node(T data, Node<T> next)
+	{
+        this.data = data;
+        this.next = next;
+	}
+}
