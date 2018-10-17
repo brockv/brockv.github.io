a89456bc4da08e7b18d2c284668332978bc6bc57
brockv <vancebrock22@gmail.com>
2018-10-17 01:44:45 -0700

    Major revisions. Fixed routing and built pages.

---
+++ CS460/HWK4/Homework4/Homework4/Controllers/ColorController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using System.Drawing;
+using System;
+
+namespace Homework4.Controllers
+{
+    public class ColorController : Controller
+    {
+        // GET: Color Mixer
+        public ActionResult ColorMixer()
+        {
+
+            string firstColor = Request.Form["firstColor"];
+            string secondColor = Request.Form["secondColor"];
+
+            Color rgbColorOne = ColorTranslator.FromHtml(firstColor);
+            Color rgbColorTwo = ColorTranslator.FromHtml(secondColor);
+
+            int finalColorAlpha = Math.Max(Math.Min(rgbColorOne.A + rgbColorTwo.A, 255), 0);
+            int finalColorRed = Math.Max(Math.Min(rgbColorOne.R + rgbColorTwo.R, 255), 0);
+            int finalColorGreen = Math.Max(Math.Min(rgbColorOne.G + rgbColorTwo.G, 255), 0);
+            int finalColorBlue = Math.Max(Math.Min(rgbColorOne.B + rgbColorTwo.B, 255), 0);
+
+            Console.WriteLine(finalColorAlpha);
+            Console.WriteLine(finalColorRed);
+            Console.WriteLine(finalColorGreen);
+            Console.WriteLine(finalColorBlue);
+
+            string finalColor = ColorTranslator.ToHtml(Color.FromArgb(finalColorAlpha, finalColorRed, finalColorGreen, finalColorBlue));
+
+            ViewBag.FirstColor = "width:160px; height: 160px; border: 2px solid #000000; display: inline-block; background: " + firstColor + "; ";
+            ViewBag.SecondColor = "width:160px; height: 160px; border: 2px solid #000000; display: inline-block; background: " + secondColor + "; ";
+            ViewBag.FinalColor = "width:160px; height: 160px; border: 2px solid #000000;  display: inline-block;background: " + finalColor + "; ";
+
+            return View();
+        }
+    }
+}
--- CS460/HWK4/Homework4/Homework4/Controllers/HomeController.cs
+++ CS460/HWK4/Homework4/Homework4/Controllers/HomeController.cs
@@ -1,29 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Homework4.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        /// <summary>
+        /// GET for Home.cshtml
+        /// </summary>
+        /// <returns>The view - Home.cshtml</returns>
         public ActionResult Home()
         {
             return View();
         }
 
-        // GET: Converter
+        /// <summary>
+        /// GET for Converter.cshtml
+        /// </summary>
+        /// <returns>The view - Converter.cshtml</returns>
         public ActionResult Converter()
-        {
-            return View();
-        }
+        {            
+            double miles = 0;
+            string userInput = Request["miles_to_convert"];
+            string unit = Request["units"];
 
-        // GET: Color Mixer
-        public ActionResult ColorMixer()
-        {
-            return View();
+            /* Attempt to parse the input we grabbed from the form */
+            if (Double.TryParse(userInput, out miles))
+            {
+                /* If we parsed it successfully, do the appropriate conversion */
+                switch (unit)
+                {
+                    case "millimeters":
+                        miles = miles * 1609344;
+                        break;
+                    case "centimeters":
+                        miles = miles * 160934.4;
+                        break;
+                    case "meters":
+                        miles = miles * 1609.34;
+                        break;
+                    case "kilometers":
+                        miles = miles * 1.60934;
+                        break;
+                    default:
+                        break;
+                }
+
+                /* Construct a message to display relating to the conversion */
+                ViewBag.Message = (userInput + " mile(s) is equal to " + miles + " " + unit + ".");
+            }
+            
+            /* Return the view (Converter.cshtml) */
+            return View();            
         }
     }
 }
