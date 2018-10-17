56588ca55cdf6f824bae8aa06328d7b0515a2d98
brockv <vancebrock22@gmail.com>
2018-10-17 11:33:49 -0700

    Major revision of Color page. Added color pickers for ease of use.

--- CS460/HWK4/Homework4/Homework4/Controllers/ColorController.cs
+++ CS460/HWK4/Homework4/Homework4/Controllers/ColorController.cs
@@ -20,31 +20,31 @@ namespace Homework4.Controllers
             string secondColor = Request.Form["secondColor"];
 
             /* Make sure the fields aren't null before proceeding */
-            if (firstColor != null && secondColor != null)
+            if (firstColor != null || secondColor != null)
             {
                 /* COnvert the strings into Color objects so we can add them together */
                 Color rgbColorOne = ColorTranslator.FromHtml(firstColor);
                 Color rgbColorTwo = ColorTranslator.FromHtml(secondColor);
 
-                /* Add each channel from the two colors, clamping the range from 0 to 255 */
-                int finalColorAlpha = Math.Max(Math.Min(rgbColorOne.A + rgbColorTwo.A, 255), 0);
-                int finalColorRed = Math.Max(Math.Min(rgbColorOne.R + rgbColorTwo.R, 255), 0);
-                int finalColorGreen = Math.Max(Math.Min(rgbColorOne.G + rgbColorTwo.G, 255), 0);
-                int finalColorBlue = Math.Max(Math.Min(rgbColorOne.B + rgbColorTwo.B, 255), 0);
+                /* Add each channel from the two colors, clamping the ranges from 0 to 255 */
+                int finalColorAlpha = Math.Min(Math.Max(rgbColorOne.A + rgbColorTwo.A, 0), 255);
+                int finalColorRed = Math.Min(Math.Max(rgbColorOne.R + rgbColorTwo.R, 0), 255);
+                int finalColorGreen = Math.Min(Math.Max(rgbColorOne.G + rgbColorTwo.G, 0), 255);
+                int finalColorBlue = Math.Min(Math.Max(rgbColorOne.B + rgbColorTwo.B, 0), 255);
 
                 /* Construct the mixture using the channels we just calculated */
                 string finalColor = ColorTranslator.ToHtml(Color.FromArgb(finalColorAlpha, finalColorRed, finalColorGreen, finalColorBlue));
 
-                /* Set the style information for each of the boxes */
-                ViewBag.FirstColor = "width:160px; height: 160px; border: 2px solid #000000; display: inline-block; background: " + firstColor + "; ";
-                ViewBag.SecondColor = "width:160px; height: 160px; border: 2px solid #000000; display: inline-block; background: " + secondColor + "; ";
-                ViewBag.FinalColor = "width:160px; height: 160px; border: 2px solid #000000;  display: inline-block;background: " + finalColor + "; ";
+                /* Set the background information for each of the boxes */
+                ViewBag.FirstColorBox = "background: " + firstColor + ";";
+                ViewBag.SecondColorBox = "background: " + secondColor + ";";
+                ViewBag.FinalColorBox = "background: " + finalColor + ";";
 
                 /* Change the flag to show the boxes */
                 ViewBag.ShowBoxes = true;
             }
 
-
+            /* Return the view, with or without the boxes visible */
             return View();
         }
     }
