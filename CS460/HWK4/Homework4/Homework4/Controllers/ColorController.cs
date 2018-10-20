691e6bdb667687baf16b4aeff533c13586f2cc73
brockv <vancebrock22@gmail.com>
2018-10-20 14:13:19 -0700

    Updated the logic in ColorController to allow wrapping color values.

--- CS460/HWK4/Homework4/Homework4/Controllers/ColorController.cs
+++ CS460/HWK4/Homework4/Homework4/Controllers/ColorController.cs
@@ -7,7 +7,7 @@ namespace Homework4.Controllers
     public class ColorController : Controller
     {
         /// <summary>
-        /// GET for Converter
+        /// GET for ColorMixer
         /// </summary>
         /// <returns>The view - ColorMixer.cshtml</returns>
         public ActionResult ColorMixer()
@@ -15,25 +15,46 @@ namespace Homework4.Controllers
             /* Set the flag to show the boxes to false initially */
             ViewBag.ShowBoxes = false;
 
-            /* Grab the color codes from the text boxes */
-            string firstColor = Request.Form["firstColor"];
-            string secondColor = Request.Form["secondColor"];
+            /* Return the view */
+            return View();
+        }
+
+        /// <summary>
+        /// POST for ColorMixer. Gets the values from the textboxes and generates a new
+        /// color by adding the ARGB color channels together.
+        /// </summary>
+        /// <param name="firstColor">The color code from the first textbox.</param>
+        /// <param name="secondColor">The color code from the second textbox.</param>
+        /// <returns>The view - ColorMixer.cshtml - with the bool flag in the appropriate state.</returns>
+        [HttpPost]
+        public ActionResult ColorMixer(string firstColor, string secondColor)
+        {
+            /* Set the flag to show the boxes to false initially */
+            ViewBag.ShowBoxes = false;
 
             /* Make sure the fields aren't null before proceeding */
-            if (firstColor != null || secondColor != null)
+            if (firstColor != null && secondColor != null)
             {
-                /* COnvert the strings into Color objects so we can add them together */
+                /* Convert the strings into Color objects so we can add them together */
                 Color rgbColorOne = ColorTranslator.FromHtml(firstColor);
                 Color rgbColorTwo = ColorTranslator.FromHtml(secondColor);
 
-                /* Add each channel from the two colors, clamping the ranges from 0 to 255 */
-                int finalColorAlpha = Math.Min(Math.Max(rgbColorOne.A + rgbColorTwo.A, 0), 255);
-                int finalColorRed = Math.Min(Math.Max(rgbColorOne.R + rgbColorTwo.R, 0), 255);
-                int finalColorGreen = Math.Min(Math.Max(rgbColorOne.G + rgbColorTwo.G, 0), 255);
-                int finalColorBlue = Math.Min(Math.Max(rgbColorOne.B + rgbColorTwo.B, 0), 255);
+                /* Add each channel together from the two colors */
+                int finalColorRed = rgbColorOne.R + rgbColorTwo.R;
+                int finalColorGreen = rgbColorOne.G + rgbColorTwo.G;
+                int finalColorBlue = rgbColorOne.B + rgbColorTwo.B;
+
+                /* 
+                 * Subtract 255 from any values greater than 255, effectively 
+                 * wrapping around from 0. This allows for more color combinations
+                 * that don't result in plain white.
+                 */
+                if (finalColorRed > 255) finalColorRed -= 255;
+                if (finalColorGreen > 255) finalColorGreen -= 255;
+                if (finalColorBlue > 255) finalColorBlue -= 255;
 
                 /* Construct the mixture using the channels we just calculated */
-                string finalColor = ColorTranslator.ToHtml(Color.FromArgb(finalColorAlpha, finalColorRed, finalColorGreen, finalColorBlue));
+                string finalColor = ColorTranslator.ToHtml(Color.FromArgb(finalColorRed, finalColorGreen, finalColorBlue));
 
                 /* Set the background information for each of the boxes */
                 ViewBag.FirstColorBox = "background: " + firstColor + ";";
